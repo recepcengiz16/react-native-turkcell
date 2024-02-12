1294f5354b3b0aa3e6026662013ae6fc7448c505
Recep <recepcengiz_96@outlook.com>
2024-02-12 17:21:44 +0300

    configurasyonları yapıldı

--- form-validation/src/components/FormWithFormik.js
+++ form-validation/src/components/FormWithFormik.js
@@ -1,10 +1,11 @@
 import { StyleSheet, TextInput, View, Button } from 'react-native'
 import React from 'react'
-import { Formik,useFormik } from 'formik';
+import { useFormik } from 'formik';
+import validations from './validation';
 
 export default function FormWithFormik() {
 
-    const {values,handleSubmit,handleChange} = useFormik({
+    const {values,handleSubmit,handleChange,errors,handleBlur,touched} = useFormik({
         initialValues:{
             username:"",
             password:"",
@@ -12,7 +13,10 @@ export default function FormWithFormik() {
         },
         onSubmit:(values)=>{
             console.log(values)
-        }
+        },
+        validationSchema:validations
+        //handleblur inputtan ayrıldığımız an, handleblur da tagden ayrılınca hata varsa göstersin. Diğer türlü bir yerde hata varsa daha diğerlerine geçmeden hepsinde gösteriyor.
+        //touched da ayrıldığımız anda hataları objenin içine alıyor
     })
 
 
@@ -21,13 +25,27 @@ export default function FormWithFormik() {
 
 
         <View style={styles.item}>
-            <TextInput style={styles.input} autoCapitalize='none' keyboardType='email-address' placeholder='Email' value={values.email} onChangeText={handleChange("email")} />
+            <TextInput style={styles.input} autoCapitalize='none' keyboardType='email-address' placeholder='Email' value={values.email} onChangeText={handleChange("email")} onBlur={handleBlur("email")}/>
+        
+            {
+                errors.email && touched.email && <Text>{errors.email}</Text>
+            }
+           
         </View>
         <View style={styles.item}>
-            <TextInput style={styles.input} placeholder='Username' value={values.username} onChangeText={handleChange("username")} />
+            <TextInput style={styles.input} placeholder='Username' value={values.username} onChangeText={handleChange("username")} onBlur={handleBlur("username")} />
+
+            {
+                errors.username && touched.username && <Text>{errors.username}</Text>
+            }
+
         </View>
         <View style={styles.item}>
-            <TextInput style={styles.input} secureTextEntry placeholder='Password' value={values.password} onChangeText={handleChange("password")} />
+            <TextInput style={styles.input} secureTextEntry placeholder='Password' value={values.password} onChangeText={handleChange("password")} onBlur={handleBlur("password")} />
+
+            {
+                errors.password && touched.password && <Text>{errors.password}</Text>
+            }
         </View>
         <View>
             <Button title='Kaydet' onPress={handleSubmit} />
