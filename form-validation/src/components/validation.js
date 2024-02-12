46431f32d08dd59407cf223af1b932b69361baf3
Recep <recepcengiz_96@outlook.com>
2024-02-12 17:30:00 +0300

    türkçe mesajlar girildi

--- form-validation/src/components/validation.js
+++ form-validation/src/components/validation.js
@@ -1,9 +1,16 @@
 import { object, string, number, date, InferType } from 'yup';
 
+const messages= {
+    username:"Bu alan zorunludur",
+    password:"Bu alan zorunludur",
+    email:"Geçerli email adresi giriniz",
+
+}
+
 const validations = object({
-  username: string().required(),
-  password: string().required().min(5),
-  email: string().email().required(),
+  username: string().required(messages.username),
+  password: string().required(messages.password).min(5),
+  email: string().email().required(messages.email),
   //age: number().required().positive().integer(),
   //website: string().url().nullable(),
  // createdOn: date().default(() => new Date()),
