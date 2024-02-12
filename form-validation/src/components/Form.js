edb21b4881291f161e2d9863c9a413620c3f62e6
Recep <recepcengiz_96@outlook.com>
2024-02-12 13:27:21 +0300

    style verildi

--- form-validation/src/components/Form.js
+++ form-validation/src/components/Form.js
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 import React from 'react'
 
 export default function Form() {
+
+    const [username, setUsername] = useState("")
+
+
   return (
-    <View>
-      <Text>Form</Text>
+    <View style={styles.container}>
+      <TextInput style={styles.input} placeholder='Username' value={username} onChangeText={(name)=>setUsername(name)} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        width:"100%",
+        padding:10
+    },
+    input:{
+        borderWidth:1,
+        padding:10,
+        fontSize:24,
+        width:"100%"
+    }
+})
