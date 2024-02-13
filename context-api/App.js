82aed0219381400cd0de62afd247106daa45dd68
Recep <recepcengiz_96@outlook.com>
2024-02-13 10:25:41 +0300

    component yönlendirmesi yapıldı

--- context-api/App.js
+++ context-api/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
+import About from './src/screens/About';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
       </Stack.Navigator>
     </NavigationContainer>
   );
