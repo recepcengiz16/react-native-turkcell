30d8d2cc5eb859ecb68d6579a205ce92bd25f04d
Recep <recepcengiz_96@outlook.com>
2024-02-13 14:12:47 +0300

    componente prop geçildi

--- context-api/src/components/Item.js
+++ context-api/src/components/Item.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Item() {
+export default function Item({user}) {
   return (
     <View>
       <Text>Item</Text>
--- context-api/src/components/List.js
+++ context-api/src/components/List.js
@@ -7,7 +7,7 @@ export default function List() {
       <FlatList 
         data={[{id:1,name:"Ceylan"},{id:2,name:"Recep"}]} 
         keyExtractor={(item)=>item.id}
-        renderItem={({item})=> <Item users={item} />}
+        renderItem={({item})=> <Item user={item} />}
       />
     </View>
   )
