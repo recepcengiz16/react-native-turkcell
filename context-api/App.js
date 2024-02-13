8eb0ddae9686de24c0779ccf270dd1245e5e72ae
Recep <recepcengiz_96@outlook.com>
2024-02-13 11:50:34 +0300

    providerlar yazıldı

--- context-api/App.js
+++ context-api/App.js
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import ThemeContext from "./context/ThemeContext"
+import {ThemeContextProvider} from "./context/ThemeContext"
 import Router from './src/Router';
 
 export default function App() {
 
  
   return ( // value değeri bu provider altındaki herhangi bir component üzerinden erişilebilir ve obje olarak hem state değerini ve fonks. gönderdik. Her yerden bu değer değişebilsin diye
-    <ThemeContext.Provider value={{theme,setTheme}}>  
+    <ThemeContextProvider>  
        <Router />
-    </ThemeContext.Provider>
+    </ThemeContextProvider>
   );
 }
 
