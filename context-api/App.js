7da10476b699ea6294921592e85a9cdd97040fa2
Recep <recepcengiz_96@outlook.com>
2024-02-13 17:21:10 +0300

    context yapısını anlamak için soru çözüoruz

--- context-api/App.js
+++ context-api/App.js
@@ -3,14 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import {ThemeContextProvider} from "./context/ThemeContext"
 import Router from './src/Router';
+import { UserContextProvider } from './context/UsersContext';
 
 export default function App() {
 
  
   return ( // value değeri bu provider altındaki herhangi bir component üzerinden erişilebilir ve obje olarak hem state değerini ve fonks. gönderdik. Her yerden bu değer değişebilsin diye
-    <ThemeContextProvider>  
-       <Router />
-    </ThemeContextProvider>
+    // <ThemeContextProvider>  
+    //    <Router />
+    // </ThemeContextProvider>
+
+    <UserContextProvider>
+        <Router />
+    </UserContextProvider>
   );
 }
 
--- context-api/src/components/List.js
+++ context-api/src/components/List.js
@@ -1,12 +1,16 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Item from './Item'
+import { useUsers } from '../../context/UsersContext';
 
 export default function List() {
+
+  const {users} = useUsers();
+
   return (
     <View>
       <FlatList 
-        data={[{id:1,name:"Ceylan"},{id:2,name:"Recep"}]} 
+        data={users} 
         keyExtractor={(item)=>item.id}
         renderItem={({item})=> <Item user={item} />}
       />
