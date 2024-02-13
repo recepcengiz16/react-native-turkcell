86fbef109406fcecce608329284be9afab091bf7
Recep <recepcengiz_96@outlook.com>
2024-02-13 17:48:48 +0300

    küçük değişiklikler yapıldı

--- context-api/src/components/Form.js
+++ context-api/src/components/Form.js
@@ -1,10 +1,32 @@
 import { StyleSheet, Text, View,TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { useUsers } from '../../context/UsersContext';
+import { v4 as uuidv4 } from 'uuid'; //benzersiz id oluşturmak için
 
 export default function Form() {
-  return (
+
+  const {setUsers} = useUsers();
+  const [name,setName] = useState("");
+
+  const handleSubmit = ()=>{
+
+    if (!name) {
+        return false;
+    }
+
+      setUsers((prev)=>[{ id:uuidv4(),name }, ...prev ]);
+      setName("");
+  }
+
+  return ( //returnKeyType='go' telefonda klavyede git butonunun yazmasını sağlıyor, enter butonu yerine
     <View>
-      <TextInput value='Test' style={styles.input} returnKeyType='go' />
+      <TextInput 
+        style={styles.input} 
+        returnKeyType='go' 
+        value={name} 
+        onChangeText={(txt)=>setName(txt)}
+        onEndEditing={handleSubmit}
+       />
     </View>
   )
 }
