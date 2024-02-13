77a3cda07867a02a60d0410ed37979958d5141e7
Recep <recepcengiz_96@outlook.com>
2024-02-13 12:10:02 +0300

    dil bilgisini görüntüledim

--- context-api/src/screens/Home.js
+++ context-api/src/screens/Home.js
@@ -1,15 +1,18 @@
 import { StyleSheet, Text, View,Button } from 'react-native'
 import React,{useContext} from 'react'
 import ThemeContext from '../../context/ThemeContext'
+import LanguageContext from '../../context/LanguageContext';
 
 export default function Home({navigation}) {
 
     const {theme} = useContext(ThemeContext);
+    const {lang}=useContext(LanguageContext);
    
 
   return (
     <View style={{ flex:1, justifyContent:"center", alignItems:"center" }}>
-      <Text> Ana Sayfa </Text>
+      <Text> Tema: {theme} </Text>
+      <Text> Dil: {lang} </Text>
       <Button title='Go to the About' onPress={()=>navigation.navigate("About")} />
     </View>
   )
