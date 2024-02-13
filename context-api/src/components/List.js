510ec46e1436dec9f0b4bdc211324a70a92f97a3
Recep <recepcengiz_96@outlook.com>
2024-02-13 14:03:01 +0300

    liste için data düzenlemelerini yaptım

---
+++ context-api/src/components/List.js
@@ -0,0 +1,16 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function List() {
+  return (
+    <View>
+      <FlatList 
+        data={[{id:1,name:"Ceylan"},{id:2,name:"Recep"}]} 
+        keyExtractor={(item)=>item.id}
+        renderItem={({item})=> <Item users={item} />}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
