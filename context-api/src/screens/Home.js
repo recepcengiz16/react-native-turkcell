c49fbcf44f2a5fb383c07d56aafddf8a05acd8d3
Recep <recepcengiz_96@outlook.com>
2024-02-13 10:43:46 +0300

    context kullanımını yaptım

--- context-api/src/screens/Home.js
+++ context-api/src/screens/Home.js
@@ -1,7 +1,12 @@
 import { StyleSheet, Text, View,Button } from 'react-native'
-import React from 'react'
+import React,{useContext} from 'react'
+import ThemeContext from '../../context/ThemeContext'
 
 export default function Home({navigation}) {
+
+    const data = useContext(ThemeContext);
+    console.log(data)
+
   return (
     <View style={{ flex:1, justifyContent:"center", alignItems:"center" }}>
       <Text> Ana Sayfa </Text>
