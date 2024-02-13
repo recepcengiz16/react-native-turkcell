1bed983ad25c7cdfcfc6c5072941fc330972b904
Recep <recepcengiz_96@outlook.com>
2024-02-13 11:35:24 +0300

    provider ve içindeki children için ayarlamalar yapıldı

--- context-api/context/ThemeContext.js
+++ context-api/context/ThemeContext.js
@@ -2,4 +2,10 @@ import { createContext } from "react";
 
 const ThemeContext= createContext();
 
+export const ThemeContextProvider = ({children})=>{ // provider altında çağırılmalı ya componentler o yüzden children olarak yazıyoruz o componentleri
+    return <ThemeContext.Provider>
+        {children} 
+    </ThemeContext.Provider>
+}
+
 export default ThemeContext;
