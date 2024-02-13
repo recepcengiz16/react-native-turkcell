af60bddf36102116aa5e3ddef6eccc4f9ef692b3
Recep <recepcengiz_96@outlook.com>
2024-02-13 10:24:36 +0300

    component yönlendirmesi yapıldı

--- context-api/App.js
+++ context-api/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
