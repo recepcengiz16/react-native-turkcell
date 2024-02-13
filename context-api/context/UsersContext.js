f5ef4e68966e58e3eb411449e3f59765d5b0404e
Recep <recepcengiz_96@outlook.com>
2024-02-13 17:16:28 +0300

    context iç yapısı düzenlendi

--- context-api/context/UsersContext.js
+++ context-api/context/UsersContext.js
@@ -1,2 +1,29 @@
-import {createContext} from "react";
+import {createContext, useContext, useState} from "react";
 
+const UserContext = createContext();
+
+export const UserContextProvider = ({children})=>{
+
+    const [users,setUsers]=useState([
+        {id:1,name:"Ceylan"},
+        {id:2,name:"Recep"}
+    ])
+
+    const values = {
+        users,
+        setUsers
+    }
+
+    return <UserContext.Provider value={values}>{children}</UserContext.Provider>
+
+}
+
+export const useUsers = ()=>{
+    const context = useContext(UserContext);
+
+    if (context===undefined) {
+        throw new Error("setUsers hook must be call inside UserContextProvider");
+    }
+
+    return context;
+}
