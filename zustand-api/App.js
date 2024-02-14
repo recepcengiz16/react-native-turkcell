915b1b797b6e82888c871e52ee61805aea1bd061
Recep <recepcengiz_96@outlook.com>
2024-02-14 09:46:14 +0300

    yorum satırı yazıldı

--- zustand-api/App.js
+++ zustand-api/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {// zustand api de context api gibi provider ve sarmalanmasına gerek yok. Her bir context için hook yazmamız gerekliydi vs.
+export default function App() {// zustand api de context api gibi provider ve sarmalanmasına gerek yok. Her bir context için hook yazmamız gerekliydi vs. Zustondla beraber herhangi bir state i kaydedip onu kaldığı yerden tekrardan kullanabiliriz. vue js deki store gibi düşünebilirsin.
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
