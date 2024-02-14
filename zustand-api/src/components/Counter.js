cd23689f873a1097c4f57a9020c86863d90dfa69
Recep <recepcengiz_96@outlook.com>
2024-02-14 10:12:23 +0300

    counter componenti oluşturuldu

---
+++ zustand-api/src/components/Counter.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React from 'react'
+
+export default function Counter() {
+  return (
+    <View>
+      <Text>Counter</Text>
+      <Button title='Artır' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
