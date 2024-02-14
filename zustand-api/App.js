2c964bc7c6485c4eaa70b6633ec1a457a8dc44a7
Recep <recepcengiz_96@outlook.com>
2024-02-14 09:49:53 +0300

    yorum satırı yazıldı

--- zustand-api/App.js
+++ zustand-api/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {// zustand api de context api gibi provider ve sarmalanmasına gerek yok. Her bir context için hook yazmamız gerekliydi vs. Zustondla beraber herhangi bir state i kaydedip onu kaldığı yerden tekrardan kullanabiliriz. vue js deki store gibi düşünebilirsin.
+export default function App() {// zustand api de context api gibi provider ve sarmalanmasına gerek yok. Her bir context için hook yazmamız gerekliydi vs. Zustondla beraber herhangi bir state i kaydedip onu kaldığı yerden tekrardan kullanabiliriz persist özelliği ile. vue js deki store gibi düşünebilirsin. Bir artısı da normalde context apide provider altındaki herhangi bir state güncellendiğinde altındaki her component tekrar render edilir.
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
