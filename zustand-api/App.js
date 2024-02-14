2d3336091c02aa13291e05438d2b636650e1c374
Recep <recepcengiz_96@outlook.com>
2024-02-14 10:14:05 +0300

    counter componenti sayfaya eklendi

--- zustand-api/App.js
+++ zustand-api/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, View } from 'react-native';
+import Counter from './src/components/Counter';
 
 export default function App() {// zustand api de context api gibi provider ve sarmalanmasına gerek yok. Her bir context için hook yazmamız gerekliydi vs. Zustondla beraber herhangi bir state i kaydedip onu kaldığı yerden tekrardan kullanabiliriz persist özelliği ile. vue js deki store gibi düşünebilirsin. Bir artısı da normalde context apide provider altındaki herhangi bir state güncellendiğinde altındaki her component tekrar render edilir.
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Counter />
     </View>
   );
 }
