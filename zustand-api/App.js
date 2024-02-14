c1aebbfc258861a1ca966b2ee0ca06ae8f9e6e8c
Recep <recepcengiz_96@outlook.com>
2024-02-14 10:19:57 +0300

    provider içerisine template yerleştirildi

--- zustand-api/App.js
+++ zustand-api/App.js
@@ -1,12 +1,15 @@
 
 import { StyleSheet, View } from 'react-native';
 import Counter from './src/components/Counter';
+import { CounterContextProvider } from './src/context/CounterContext';
 
 export default function App() {// zustand api de context api gibi provider ve sarmalanmasına gerek yok. Her bir context için hook yazmamız gerekliydi vs. Zustondla beraber herhangi bir state i kaydedip onu kaldığı yerden tekrardan kullanabiliriz persist özelliği ile. vue js deki store gibi düşünebilirsin. Bir artısı da normalde context apide provider altındaki herhangi bir state güncellendiğinde altındaki her component tekrar render edilir.
   return (
-    <View style={styles.container}>
-       <Counter />
-    </View>
+    <CounterContextProvider>
+        <View style={styles.container}>
+          <Counter />
+        </View>
+    </CounterContextProvider>
   );
 }
 
