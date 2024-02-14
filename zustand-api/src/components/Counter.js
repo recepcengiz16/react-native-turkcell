e248a6f06e83f29800baa309e826449d4da18628
Recep <recepcengiz_96@outlook.com>
2024-02-14 10:25:11 +0300

    context kullanarak count değerinin artırılması işlemi

--- zustand-api/src/components/Counter.js
+++ zustand-api/src/components/Counter.js
@@ -4,12 +4,12 @@ import { useCounter } from '../context/CounterContext'
 
 export default function Counter() {
 
-    const {count} = useCounter();
+    const {count,setCount} = useCounter();
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}> {count} </Text>
-      <Button title='Artır' />
+      <Button title='Artır' onPress={ () => setCount(count+1) } />
     </View>
   )
 }
