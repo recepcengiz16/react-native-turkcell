7d89c7f021f5af3c071ab5ea9f468a0fe02ed284
Recep <recepcengiz_96@outlook.com>
2024-02-14 08:52:58 +0300

    Mesaj componenti içeriği düzenlendi

--- context-api/src/components/Message.js
+++ context-api/src/components/Message.js
@@ -1,12 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import PropType from "prop-types"
 
-export default function Message() {
+export default function Message({bgColor,message}) {
   return (
-    <View>
-      <Text>Message</Text>
+    <View style={[styles.container,{backgroundColor:bgColor}]}>
+      <Text style={styles.text}>{message}</Text>
+
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+Message.prototype = {
+    bgColor: PropType.string,
+    message: PropType.string
+}
+
+Message.defaultProps =  {
+    bgColor:"orange"
+}
+
+const styles = StyleSheet.create({
+    container:{
+        borderColor:"#ddd",
+        borderWidth:1,
+        margin:10
+    },
+
+    text:{
+        fontSize:16
+    }
+})
