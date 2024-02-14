0a66ebfdf1097427ca9cf4c166ced3f69867c7f8
Recep <recepcengiz_96@outlook.com>
2024-02-14 11:26:00 +0300

    yorum satırı yazıldı

--- zustand-api/src/components/CounterWidthZustand.js
+++ zustand-api/src/components/CounterWidthZustand.js
@@ -4,11 +4,15 @@ import useStore from '../store/useStore'
 
 export default function CounterWidthZustand() {
 
-    const data = useStore();
+   // const data = useStore(); içerisinde eğer başka state de varsa o da değişirse yine component render edilir. Bu da sıkıntılı. Bu yüzden alttaki gibi kullanacağımız şeyleri yazmamız lazım
+
+   const count = useStore(state=>state.count);
+   const increase = useStore(state=>state.increase);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> {data.count} </Text>
+      <Text style={styles.text}> {count} </Text>
+      <Button title='Artır' onPress={ increase } />
       {/* <Button title='Artır' onPress={ () => setCount(count+1) } /> */}
     </View>
   )
