027371800501d436b4b1b5499beff4406af92ae3
Recep <recepcengiz_96@outlook.com>
2024-02-14 10:19:00 +0300

    context içindeki bilgilerin kullanılması için düzenleme yapıldı

--- zustand-api/src/components/Counter.js
+++ zustand-api/src/components/Counter.js
@@ -1,13 +1,25 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
+import { useCounter } from '../context/CounterContext'
 
 export default function Counter() {
+
+    const {count} = useCounter();
+
   return (
-    <View>
-      <Text>Counter</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}> {count} </Text>
       <Button title='Artır' />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    text:{
+        fontSize:16
+    }
+})
