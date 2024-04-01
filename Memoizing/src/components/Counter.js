09a42b65910f9fa0b66d2cb7bb5958a3faaf7e2a
Recep <recepcengiz_96@outlook.com>
2024-04-01 13:21:40 +0300

    yorum satırı eklendi

--- Memoizing/src/components/Counter.js
+++ Memoizing/src/components/Counter.js
@@ -1,15 +1,24 @@
 import { StyleSheet, Text, View,Button } from 'react-native'
-import React from 'react'
+import React,{useMemo} from 'react'
 import Header from './Header';
 
 export default function Counter() {
 
     const [count, setCount] = useState(0);
 
+    const user = useMemo(()=>{
+      return {
+        id:1,
+        name:"Recep",
+        count
+      }
+    },[count]); //1. parametre arrow fonksiyon, 2. parametre de bağımlı değişkenlerin olduğu array. count değeri değişirse. useEffect gibi düşün bunu da aynı eğer boş array olsaydı bu component mount edildiğinde user oluşacaktı, şimdi de count değişince user objesi değişecek
 
   return (
     <View style={styles.container}>
       <Header />
+
+      <User user={user} />
 
       <Text>{count}</Text>
       <Button title='Artır' onPress={()=>setCount(count+1)} />
--- Memoizing/src/components/User.js
+++ Memoizing/src/components/User.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const User = () => {
+const User = ({user}) => {
   return (
     <View>
       <Text>User</Text>
+      <Text>
+        {
+            JSON.stringify(user)
+        }
+      </Text>
     </View>
   )
 }
