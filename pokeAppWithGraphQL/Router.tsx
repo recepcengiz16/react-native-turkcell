56f6a028705eef4e07fa8a06f84758849d0fb739
Recep <recepcengiz_96@outlook.com>
2024-04-02 16:32:59 +0300

    navigation paketleri yüklendi

---
+++ pokeAppWithGraphQL/Router.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Router() {
+  return (
+    <View>
+      <Text>Router</Text>
+    </View>
+  )
+}
