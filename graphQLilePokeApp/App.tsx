1e4fb7a5e6e5c2a3a934f5185ebd7349ab99f3ae
Recep <recepcengiz_96@outlook.com>
2024-04-02 10:11:25 +0300

    graphql kullanımını öğrenmek için uygulama oluşturuldu

---
+++ graphQLilePokeApp/App.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export default function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
