02ca8de430daed6f725e81788bad6a83c7f7829d
Recep <recepcengiz_96@outlook.com>
2024-04-02 16:37:18 +0300

    poemonların listelendiği sayfa eklendi

--- pokeAppWithGraphQL/Router.tsx
+++ pokeAppWithGraphQL/Router.tsx
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//screens
+import Pokemons from './src/screens/Pokemons';
+
+const Stack = createNativeStackNavigator();
 
 export default function Router() {
   return (
-    <View>
-      <Text>Router</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Pokemons" component={Pokemons} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
---
+++ pokeAppWithGraphQL/src/screens/Pokemons/Pokemons.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import styles from "./Pokemons.style";
+
+export default function Pokemons() {
+  return (
+    <View>
+      <Text>Pokemons</Text>
+    </View>
+  )
+}
