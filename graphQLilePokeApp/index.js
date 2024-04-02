eabd83a2d7c3f2849eec2ef1ad734314aa85a175
Recep <recepcengiz_96@outlook.com>
2024-04-02 11:08:13 +0300

    graphql ayarlamaları yapıldı

--- graphQLilePokeApp/index.js
+++ graphQLilePokeApp/index.js
@@ -3,7 +3,24 @@
  */
 
 import {AppRegistry} from 'react-native';
+
+import {ApolloProvider} from "@apollo/react-hooks";
+import { ApolloClient, HttpLink } from 'apollo-boost';
+
 import Router from './Router';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Router);
+const client = new ApolloClient({
+    link: new HttpLink({
+        uri:"https://grapql-pokemon.now.sh"
+    }),
+    cache:new InMemoryCache()
+}) 
+
+const App = ()=> {
+    <ApolloProvider client={client}>
+        <Router />
+    </ApolloProvider>
+}
+
+AppRegistry.registerComponent(appName, () => App);
