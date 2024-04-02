2fcaaa1103a7613a9fefd97a9e304aa5c61ebf46
Recep <recepcengiz_96@outlook.com>
2024-04-02 13:32:24 +0300

    sayfada query ayarlamaları yapıldı

--- graphQLilePokeApp/index.js
+++ graphQLilePokeApp/index.js
@@ -3,24 +3,24 @@
  */
 
 import {AppRegistry} from 'react-native';
-
-import {ApolloProvider} from "@apollo/react-hooks";
-import { ApolloClient, HttpLink } from 'apollo-boost';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import Router from './Router';
 import {name as appName} from './app.json';
 
+
 const client = new ApolloClient({
-    link: new HttpLink({
-        uri:"https://grapql-pokemon.now.sh"
-    }),
-    cache:new InMemoryCache()
-}) 
+    uri: 'https://graphql-pokemon.vercel.app/',
+    cache: new InMemoryCache()
+  });
 
 const App = ()=> {
+   
     <ApolloProvider client={client}>
         <Router />
     </ApolloProvider>
+    
 }
 
-AppRegistry.registerComponent(appName, () => App);
+//AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Router);
--- graphQLilePokeApp/src/screens/Pokemons/Pokemons.tsx
+++ graphQLilePokeApp/src/screens/Pokemons/Pokemons.tsx
@@ -1,11 +1,46 @@
-import {Text, View} from 'react-native';
+import {ActivityIndicator, Text, View} from 'react-native';
 import React from 'react';
-import styles from "./Pokemons.style"
+
+import {useQuery} from '@apollo/client';
+import {gql} from '@apollo/client';
+
+const pokemon_query = gql`
+  {
+    pokemons(first:10){
+      id
+      name
+    }
+  }
+`;
 
 export default function Pokemons(): React.JSX.Element {
+
+  const {error,loading,data} = useQuery(pokemon_query);
+
+  if (loading) {
+    return (
+      <View>
+         <ActivityIndicator size={25} />
+      </View>
+    )
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Error..</Text>
+      </View>
+    )
+  }
+
   return (
     <View>
-      <Text>Pokemons</Text>
+      {/* <FlatList
+        data={data.pokemons}
+        renderItem={({item})=><Text>{item.name}</Text>}
+        keyExtractor={(item)=>item.id}
+      /> */}
+      <Text>Recep</Text>
     </View>
   );
 }
