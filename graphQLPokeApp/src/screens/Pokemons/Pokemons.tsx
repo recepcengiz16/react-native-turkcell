18580af68e9f51300293bf4b8f74852d30111835
Recep <recepcengiz_96@outlook.com>
2024-04-03 09:14:26 +0300

    pokemonların listelendiği sayfalar oluşturuldu

---
+++ graphQLPokeApp/src/screens/Pokemons/Pokemons.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import styles from "./Pokemons.style";
+
+export default function Pokemons() {
+  return (
+    <View>
+      <Text>Pokemons</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
